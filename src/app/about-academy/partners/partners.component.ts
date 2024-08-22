f18f90fa880910efc61060729f9a4890c93f6ee2
afawzy.c <afawzy.c@NEC.GOV.SA>
2024-08-21 19:30:36 -0700

    add some pages

--- src/app/about-academy/partners/partners.component.ts
+++ src/app/about-academy/partners/partners.component.ts
@@ -1,15 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-partners',
   templateUrl: './partners.component.html',
   styleUrls: ['./partners.component.scss']
 })
-export class PartnersComponent implements OnInit {
+export class PartnersComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  partners = []
+
+  constructor() { 
+    this.partners = [
+      { 
+        img: "assets/images/partners/helwan.png", 
+        Partner: "جامعة حلوان",
+        divElement: 'helwan'
+      },
+      { 
+        img: "assets/images/partners/adults.png", 
+        Partner: "جهاز تشغيل شباب الخريجين",
+        divElement: 'adults'
+      },
+      { 
+        img: "assets/images/partners/assiut.png", 
+        Partner: "جامعة اسيوط",
+        divElement: 'assiut'
+      },
+      { 
+        img: "assets/images/partners/dubai.png", 
+        Partner: "حكومة دبي",
+        divElement: 'dubai'
+      },
+      { 
+        img: "assets/images/partners/investigation.png", 
+        Partner: "وزارة الاستثمار و التعاون الدولي",
+        divElement: 'investigation'
+      },
+      { 
+        img: "assets/images/partners/azhar.png", 
+        Partner: "جامعة الازهر",
+        divElement: 'azhar'
+      },
+      { 
+        img: "assets/images/partners/benha.png", 
+        Partner: "جامعة بنها",
+        divElement: 'benha'
+      },
+      { 
+        img: "assets/images/partners/watny.png", 
+        Partner: "المجلس الوطني للتدريب و التعليم",
+        divElement: 'watny'
+      },
+      { 
+        img: "assets/images/partners/cairo.png", 
+        Partner: "جامعة القاهرة",
+        divElement: 'cairo'
+      },
+    ];
+  }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    document.getElementById('partnerDefaultOpen').click()
+  }
+
+  openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
   }
 
 }
